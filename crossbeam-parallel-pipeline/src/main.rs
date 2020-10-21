b73b9de01dafe1a40a9e44457e6bb9b16ee59a6d
Ratnadeep Bhattacharya <ratnadeepb@nimbnode19.nimbus.seas.gwu.edu>
2020-10-21 16:30:56 +0000

    adding crossbeam pipeline

--- crossbeam-parallel-pipeline/src/main.rs
+++ crossbeam-parallel-pipeline/src/main.rs
@@ -1,3 +1,43 @@
+use crossbeam;
+use crossbeam_channel::bounded;
+use std::{thread, time};
+
 fn main() {
-    println!("Hello, world!");
+    // a channel with a buffer of size 1
+    let (snd1, rcv1) = bounded(1);
+    let (snd2, rcv2) = bounded(1);
+    let n_msgs = 4;
+    let n_workers = 2;
+
+    crossbeam::scope(|s| {
+        // producer thread
+        s.spawn(|_| {
+            for i in 0..n_msgs {
+                snd1.send(i).unwrap();
+                println!("source sent {}", i);
+            }
+            drop(snd1); // close the channel
+        });
+        // parallel processing by two threads
+        for _ in 0..n_workers {
+            // send to sink, receive from source
+            let (sndr, rcvr) = (snd2.clone(), rcv1.clone());
+            // spawn workers in separate threads
+            s.spawn(move |_| {
+                thread::sleep(time::Duration::from_millis(500));
+                // receive until channel closes
+                for msg in rcvr.iter() {
+                    println!("Worker {:?} received {}", thread::current().id(), msg);
+                    sndr.send(msg * 2).unwrap();
+                }
+            });
+        }
+        // close channel, otherwise sink will not exit the for-loop
+        drop(snd2);
+        // sink
+        for msg in rcv2.iter() {
+            println!("Sink received {}", msg);
+        }
+    })
+    .unwrap();
 }
